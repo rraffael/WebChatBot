01fec44303e4a36f3910b6ff2eb270ee928f3d8a
Leonardo Machado Leite <38584731+LeonardoMachadoLeite@users.noreply.github.com>
2019-03-31 16:14:20 -0300

    Adição Classes do outro projeto

---
+++ learningspace_web_bot/Domain/Models/InputReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace App2
+{
+    public class InputReader
+    {
+        public string GetInput(string inputJson)
+        {
+            string output = "";
+            //string json = "{\"name\":\"Anthony\",\"dialog\":\"Alou\"}";    /// teste
+
+            JObject jObject = JObject.Parse(inputJson);
+            string name = (string)jObject.SelectToken("name");
+            string dialog = (string)jObject.SelectToken("dialog");
+            output = dialog;
+
+            return output;
+        }
+    }
+}
+
+
